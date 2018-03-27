83776b3c20a5700ca18a97f00c29833c91cdd27b
boming <boming0529@gmail.com>
2018-03-27 12:18:57 +0800

    [FIX] dobor_todolist_project_task_timesheet : migration odoo 10
        > add _get_timesheet_cost for account_id
        > add wizard for default_value

--- dobtor_todolist_project_task_timesheet/models/dobtor_todolist_project_task_timesheet.py
+++ dobtor_todolist_project_task_timesheet/models/dobtor_todolist_project_task_timesheet.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api
-from openerp.exceptions import Warning as UserError
-from openerp.tools.translate import _ 
-from openerp.addons.base.res import res_request
+from odoo import models, fields, api
+from odoo.exceptions import Warning as UserError
+from odoo.addons.base.res import res_request
+from odoo.addons.sale_timesheet.models.account import AccountAnalyticLine
 
 class DobtorTodoListCore(models.Model):
     _inherit = 'dobtor.todolist.core'
+
+    @api.multi
+    def _get_default_analytic_account_id(self):
+        res = self.env['account.analytic.account'].search(
+            [('name', '=', 'Undefined')])
+        return res and res[0] or False
 
     remaining_hours = fields.Float(
         compute='_hours_get', string="Remaining Hours", store=True, default=0, 
@@ -24,9 +30,8 @@ class DobtorTodoListCore(models.Model):
         compute='_hours_get', string='Delay Hours', default=0, store=True,
         help="Computed as difference between planned hours by the project manager and the total hours of the task.")
     timesheet_ids = fields.One2many('account.analytic.line', 'todo_id', 'Timesheets')
-    #analytic_account_id = fields.Many2one('account.analytic.account', compute='_get_default_analytic_account_id', store=True)
     analytic_account_id = fields.Many2one('account.analytic.account',
-                                          'Analytic Account', default='_get_default_analytic_account_id', store=True)
+                                          'Analytic Account', default=_get_default_analytic_account_id, store=True)
 
     @api.onchange('planned_hours', 'remaining_hours', 'timesheet_ids')
     def change_unit_amount(self):
@@ -36,10 +41,7 @@ class DobtorTodoListCore(models.Model):
     def change_parent(self):
         super(DobtorTodoListCore, self).change_parent()
 
-    @api.multi
-    def _get_default_analytic_account_id(self):
-        res = self.env['account.analytic.account'].search([('name', '=', 'Undefined')])
-        return res and res[0] or False
+
 
     @api.multi
     def _hours_get(self):
@@ -61,11 +63,21 @@ class DobtorTodoListCore(models.Model):
         self.ensure_one()
         default_ref_model = None
         default_parent_model = None
+        default_project_id = None
+        default_task_id = None
+        account_id = self.env['account.analytic.account'].search([('name', '=', 'Undefined')])
         if self.ref_model:
             default_ref_model = self.ref_name + "," + str(self.ref_id)
+            if self.ref_name == 'project.task':
+                default_task_id = self.ref_id
         if self.parent_model:
             default_parent_model = self.parent_name + "," + str(self.parent_id)
-        account_id = self.env['account.analytic.account'].search([('name', '=', 'Undefined')])
+            if self.parent_name == 'project.project':
+                default_project_id = self.parent_id
+                account_id = self.env['project.project'].search(
+                    [('id', '=', self.parent_id)])[0].analytic_account_id.id
+    
+        
         res = {
             'type': 'ir.actions.act_window',
             'view_id': self.env.ref('dobtor_todolist_project_task_timesheet.dobtor_todolist_project_task_timesheet_wizard').id,
@@ -74,47 +86,53 @@ class DobtorTodoListCore(models.Model):
             'res_model': 'account.analytic.line',
             'view_type': 'form',
             'view_mode': 'form',
-            # 'context': {
-            #     'default_ref_model': default_ref_model, 
-            #     'default_parent_model': default_parent_model}
+
             'context': {
                 'default_user_id': self.env.user.id,
                 'default_todo_id': self.id,
-                'default_is_timesheet': True,
                 'default_account_id': account_id and account_id[0].id or False,
                 'default_todo_ref': default_ref_model,
                 'default_todo_ref_parent': default_parent_model,
+                'default_project_id': default_project_id,
+                'default_task_id': default_task_id,
             }
         }
         return res
 
-def referencable_models(self):
-    return res_request.referencable_models(
-        self, self.env.cr, self.env.uid, context=self.env.context)
 
-class account_analytic_line(models.Model):
+def referenceable_models(self):
+    return res_request.referenceable_models(self)
+
+
+class AccountAnalyticLine(AccountAnalyticLine):
     _inherit = "account.analytic.line"
 
     todo_id = fields.Many2one('dobtor.todolist.core', 'Todo')
-    todo_ref = fields.Reference(referencable_models, "Refer To", default=None)
-    todo_ref_parent = fields.Reference(referencable_models, "Parent", default=None)
+    todo_ref = fields.Reference(referenceable_models, "Refer To", default=None)
+    todo_ref_parent = fields.Reference(referenceable_models, "Parent", default=None)
+
+    def _get_timesheet_cost(self, values):
+        values = values if values is not None else {}
+        result = dict(values or {})
+        if (values.get('project_id') or self.project_id) or (values.get('account_id') or self.account_id):
+            if values.get('amount'):
+                return {}
+            unit_amount = values.get('unit_amount', 0.0) or self.unit_amount
+            user_id = values.get(
+                'user_id') or self.user_id.id or self._default_user()
+            user = self.env['res.users'].browse([user_id])
+            emp = self.env['hr.employee'].search(
+                [('user_id', '=', user_id)], limit=1)
+            cost = emp and emp.timesheet_cost or 0.0
+            uom = (emp or user).company_id.project_time_mode_id
+            # Nominal employee cost = 1 * company project UoM (project_time_mode_id)
+            return {
+                'amount': -unit_amount * cost,
+                'product_uom_id': uom.id,
+                'account_id': values.get('account_id') or self.account_id.id or emp.account_id.id,
+            }
+        return {}
 
     @api.multi
     def submit_time_sheet(self):
         pass
-        # print('submit_time_sheet - 1')
-        # print(self.analytic_account_id.id)
-        # print(self.name)
-        # print(self.self.id)
-        # print(self.user_id)
-        # print(fields.Datetime.now())
-        # print('submit_time_sheet - 2')
-        # self.env['account.analytic.line'].create({
-        #     'unit_amount': 0,
-        #     'account_id': self.analytic_account_id.id,
-        #     'name': self.name,
-        #     'is_timesheet': True,
-        #     'date': fields.Datetime.now(),
-        #     'todo_id': self.id,
-        #     'user_id': self.user_id
-        # })
