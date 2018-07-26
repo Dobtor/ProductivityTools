a041a79dca88a7c0832d6979c584857368a50e12
boming <boming0529@gmail.com>
2018-07-26 14:35:12 +0800

    [REM][MOV] dobtor_crm_opportunity_template : removing crm_lead template and moving into super module

--- dobtor_todolist_crm_opportunity/__manifest__.py
+++ dobtor_todolist_crm_opportunity/__manifest__.py
@@ -1,36 +1,21 @@
 # -*- coding: utf-8 -*-
 {
     'name': "dobtor_todolist_crm_opportunity",
-
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+        with crm opportunity template
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'description': """
+        with crm opportunity template
+    """,
+    'author': "dobtor SI",
+    'website': "http://www.dobtor.com",
+    'category': 'crm',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base','crm','sale','dobtor_todolist_core'],
-
+    'depends': ['crm','sale','dobtor_todolist_core'],
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/dobtor_todolist_crm_opportunity_view.xml',
         'views/dobtor_todolist_crm_opportunity_data.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+        'views/setting_crm_lead_view.xml',
     ],
 }
--- dobtor_todolist_crm_opportunity/models/__init__.py
+++ dobtor_todolist_crm_opportunity/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import dobtor_todolist_crm_opportunity
+from . import crm_lead
