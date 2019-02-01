f638602226b317967b69240910597f0798eafe71
boming <boming0529@gmail.com>
2019-02-01 13:12:09 +0800

    [Fix] flow_calender_project_issue : translate issue

--- flow_calendar_project_issue/models/calendar.py
+++ flow_calendar_project_issue/models/calendar.py
@@ -111,8 +111,8 @@ class Attendee(models.Model):
             lis = soup.find_all('li')
             need_update = False
             for li in lis:
-                if li.text.replace(' ', '') == '${object.event_id.description}':
-                    li.string = '${object.event_id.description|safe}'
+                if li.text == _('Description: ') + '${object.event_id.description}':
+                    li.string = _('Description: ') + '${object.event_id.description|safe}'
                     need_update = True
             if need_update:
                 template.update({
