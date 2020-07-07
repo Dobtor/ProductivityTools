1b5639692c2e9d321e096dc208a0844320a1a2fd
kevin.zhu <kevin.zhu@dobtor.com>
2020-07-07 11:00:40 +0800

    [MOV]移動dobtor_project_core至ProductivityTools

---
+++ dobtor_project_core/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "dobtor_project_core",
+    'summary': """
+        project interface
+    """,
+    'description': """
+        project interface :
+
+        _get_attachment_domain() interface
+    """,
+    'author': "Dobtor SI",
+    'website': "http://www.dobtor.com",
+    'category': 'dobtor',
+    'version': '0.1',
+    'depends': [
+        'project',
+    ],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/ir_attachment_view.xml',
+    ],
+}
